eaa0a632dba13027d123b540fce085d56d7feceb
David Montes <davidpmontes86@gmail.com>
2020-06-04 22:28:25 -0700

    main menu focal length

---
+++ Assets/MeshMaker/Files/LevelEditor/Scripts/LevelEditorGizmos.cs
@@ -0,0 +1,38 @@
+﻿#if UNITY_EDITOR
+
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Reflection;
+using System.Linq;
+using System.IO;
+using System.Xml;
+using System.Diagnostics;
+
+#pragma warning disable 0618
+#pragma warning disable 0168
+#pragma warning disable 0219
+#pragma warning disable 0414
+#pragma warning disable 0162
+
+namespace MeshMakerNamespace
+{
+	public class LevelEditorGizmos : MonoBehaviour
+	{
+		public void OnEnable()
+		{
+		}
+	
+		public void OnDisable()
+		{
+		}
+		
+		public void OnDestroy()
+		{
+		}
+	}
+}
+
+#endif
